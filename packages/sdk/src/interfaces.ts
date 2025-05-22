3f453639a50589651e434b60d7096780adc42d64
MarkacRobi <robi.markac@gmail.com>
2025-05-22 23:34:37 +0200

    chore: renaming, script removal

--- packages/sdk/src/interfaces.ts
+++ packages/sdk/src/interfaces.ts
@@ -3,6 +3,6 @@ import type { Address, EvmRawTransaction, EvmRawTransactionReceipt, Hash, Hex } 
 export interface IEvmWalletProvider {
   getWalletAddress: () => Address;
   getWalletAddressBytes: () => Hex;
-  sendTransaction: (config: EvmRawTransaction) => Promise<Hash>;
+  sendTransaction: (evmRawTx: EvmRawTransaction) => Promise<Hash>;
   waitForTransactionReceipt: (txHash: Hash) => Promise<EvmRawTransactionReceipt>;
 }
