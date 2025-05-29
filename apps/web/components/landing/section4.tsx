db1d6837a2066b35fb2fbe58fb8e6c4c7bb6b414
swiftcc624 <swiftcc624@gmail.com>
2025-05-29 18:38:53 +0800

    refactor: update Section4 layout and content
    - Changed layout direction to flex-col-reverse for better visual flow.
    - Updated background image and text to reflect the new event name "Stellar Summer Rewards".
    - Modified button text to "Start earning" for clarity.
    - Added new SVG icon for branding consistency.

--- apps/web/components/landing/section4.tsx
+++ apps/web/components/landing/section4.tsx
@@ -5,34 +5,27 @@ import Image from 'next/image';
 
 const Section4 = () => {
   return (
-    <div className="flex flex-col lg:flex-row section4 gap-4">
-      <div className="w-full lg:w-1/2 flex flex-col gap-2 items-center pt-[40px] sm:pt-[60px] h-[440px] sm:h-[480px] bg-[radial-gradient(circle_at_center,_#5C3623,_#1C1C24)] relative z-1">
+    <div className="flex flex-col-reverse lg:flex-row section4 gap-4 ">
+      <div className="w-full lg:w-1/2 flex flex-col gap-2 items-center pt-[40px] sm:pt-[60px] h-[440px] sm:h-[480px] bg-[#9A6DDD] relative z-1">
         <Image
           className="absolute bottom-0 left-1/2 transform -translate-x-1/2 w-[398px] z-2"
-          src="/section3-1.png"
+          src="/stellar_summar_rewards.png"
           alt="background"
           width={398}
           height={185}
         />
-        <div className="flex items-center">
-          <Image src="/symbol_dark.png" alt="SODAX Symbol" width={32} height={32} />
-          <span className="text-[24px] md:text-[32px] font-[InterRegular] font-[600] leading-[110%] ml-5 text-orange-sonic">
-            Sonic Summit
+        <div className="flex items-center gap-4">
+          <Image src="/hana.svg" alt="SODAX Symbol" width={32} height={32} />
+          <span className="text-[24px] md:text-[32px] font-[InterRegular] font-[600] leading-[110%] text-white">
+            Stellar Summer Rewards
           </span>
         </div>
-        <Label className="text-[14px] font-[500] font-[InterRegular] text-white">
-          Update copy to reflect the event took place.
-        </Label>
+        <Label className="text-[14px] font-[500] font-[InterRegular] text-white">$30,000 up for grabs.</Label>
         <div className="mt-4">
           <Button className="mr-2 rounded-full w-[133px]" variant="subtle" size="lg">
-            Main CTA
-          </Button>
-          <Button variant="ghost" className="px-6 font-['InterMedium']" size="lg">
-            Secondary CTA
+            Start earning
           </Button>
         </div>
-        <Label className="text-[17px] font-bold font-[InterRegular] text-cherry-bright mt-16">6-8 Vienna</Label>
-        <Label className="text-[46px] font-bold font-[InterRegular] text-orange-sonic leading-none">MAY</Label>
       </div>
       <div className="w-full lg:w-1/2 flex flex-col gap-2 items-center pt-[40px] sm:pt-[60px] bg-yellow-dark  h-[440px] sm:h-[480px] relative z-1">
         <Image
