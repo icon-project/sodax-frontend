3c0bfec22f1e691d0dfafedf2b2b2a88f8dda3f8
0xmilktea <0xmilktea@gmail.com>
2025-06-10 15:58:17 +0800

    chore(wallet-sdk): update package dependencies and remove unused code in constants and utils

--- packages/wallet-sdk/src/utils/index.ts
+++ packages/wallet-sdk/src/utils/index.ts
@@ -1,46 +1,4 @@
-// import { xChainMap } from '@/constants/xChains';
-
 import type { XChainId, XToken } from '@/types';
-
-// export const getNetworkDisplayName = (chain: XChainId) => {
-//   return xChainMap[chain].name;
-// };
-
-// export const ONE_DAY_DURATION = 86400000;
-
-// export function sleep(ms: number) {
-//   return new Promise((resolve) => setTimeout(resolve, ms));
-// }
-
-// export const showMessageOnBeforeUnload = (e) => {
-//   e.preventDefault();
-//   window.removeEventListener('beforeunload', showMessageOnBeforeUnload);
-//   e.returnValue = 'Your transaction will be canceled, and you’ll need to sign in again.';
-//   return e.returnValue;
-// };
-
-// export const getTrackerLink = (
-//   xChainId: XChainId,
-//   data: string,
-//   type: 'transaction' | 'address' | 'block' | 'contract' = 'transaction',
-// ) => {
-//   const tracker = xChainMap[xChainId].tracker;
-
-//   switch (type) {
-//     case 'transaction': {
-//       return `${tracker.tx}/${data}`;
-//     }
-//     case 'address': {
-//       return ``;
-//     }
-//     case 'block': {
-//       return ``;
-//     }
-//     default: {
-//       return ``;
-//     }
-//   }
-// };
 
 export const isNativeToken = (xToken: XToken) => {
   const nativeAddresses = [
