02cb8cf05acd689234cbd0337536b5a7121447b4
0xmilktea <0xmilktea@gmail.com>
2025-06-27 23:51:43 +0800

    feat: implement injective demo

--- packages/dapp-kit/src/hooks/provider/useHubProvider.ts
+++ packages/dapp-kit/src/hooks/provider/useHubProvider.ts
@@ -1,26 +1,8 @@
-import { EvmHubProvider, getHubChainConfig, type HubChainId } from '@sodax/sdk';
-import { getXChainType } from '@sodax/wallet-sdk';
-import { useMemo } from 'react';
+import type { EvmHubProvider } from '@sodax/sdk';
 import { useSodaxContext } from '../shared/useSodaxContext';
 
 export function useHubProvider(): EvmHubProvider | undefined {
-  const { sodax } = useSodaxContext();
-  const hubChainId = sodax.config?.hubProviderConfig?.chainConfig.chain.id;
-  const hubRpcUrl = sodax.config?.hubProviderConfig?.hubRpcUrl;
-  const xChainType = getXChainType(hubChainId);
-  const hubProvider = useMemo(() => {
-    if (xChainType === 'EVM' && hubChainId && hubRpcUrl) {
-      const hubChainCfg = getHubChainConfig(hubChainId);
-
-      if (!hubChainCfg) return undefined;
-
-      return new EvmHubProvider({
-        hubRpcUrl: hubRpcUrl,
-        chainConfig: hubChainCfg,
-      });
-    }
-    return undefined;
-  }, [xChainType, hubChainId, hubRpcUrl]);
+  const { hubProvider } = useSodaxContext();
 
   return hubProvider;
 }
