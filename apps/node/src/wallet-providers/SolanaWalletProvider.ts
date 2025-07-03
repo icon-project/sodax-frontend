60334435efdb7ec10033ecc2e62840187f510365
MarkacRobi <robi.markac@gmail.com>
2025-07-03 21:33:42 +0200

    feat(sdk,types,node): solana wallet interface + implementation provider

--- apps/node/src/wallet-providers/SolanaWalletProvider.ts
+++ apps/node/src/wallet-providers/SolanaWalletProvider.ts
@@ -1,28 +1,63 @@
 import { getAssociatedTokenAddress } from '@solana/spl-token';
 import {
+  type Commitment,
   Connection,
   Keypair,
-  type PublicKey,
-  type TransactionInstruction,
+  PublicKey,
+  type RpcResponseAndContext,
+  type TokenAmount,
+  TransactionInstruction,
   TransactionMessage,
   VersionedTransaction,
 } from '@solana/web3.js';
-import type { Hash, Hex } from 'viem';
-import type { ISolanaWalletProvider, SolanaWalletConfig, } from '@sodax/types';
+import type {
+  Hex,
+  ISolanaWalletProvider,
+  SolanaAccountMeta,
+  SolanaBase58PublicKey,
+  SolanaRpcResponseAndContext,
+  SolanaSerializedTransaction,
+  SolanaSignatureResult,
+  SolanaRawTransactionInstruction,
+  TransactionSignature,
+} from '@sodax/types';
+
+export type SolanaPkWalletConfig = {
+  privateKey: Uint8Array;
+  endpoint: string;
+};
+
+export type SolanaInstancedWalletConfig = {
+  wallet: Keypair;
+  connection: Connection;
+};
+
+export type SolanaWalletConfig = SolanaPkWalletConfig | SolanaInstancedWalletConfig;
+
+function isPkWalletConfig(walletConfig: SolanaWalletConfig): walletConfig is SolanaPkWalletConfig {
+  return 'privateKey' in walletConfig;
+}
 
 export class SolanaWalletProvider implements ISolanaWalletProvider {
   private readonly wallet: Keypair;
   public readonly connection: Connection;
 
-  constructor(walletConfig: SolanaWalletConfig, url: string) {
-    this.wallet = Keypair.fromSecretKey(walletConfig.privateKey);
-    this.connection = new Connection(url, 'confirmed');
+  constructor(walletConfig: SolanaWalletConfig) {
+    if (isPkWalletConfig(walletConfig)) {
+      this.wallet = Keypair.fromSecretKey(walletConfig.privateKey);
+      this.connection = new Connection(walletConfig.endpoint, 'confirmed');
+    } else {
+      this.wallet = walletConfig.wallet;
+      this.connection = walletConfig.connection;
+    }
   }
 
-  private async waitForConfirmation(signature: string, connection: Connection) {
-    const commitment = 'finalized';
-    const latestBlockhash = await connection.getLatestBlockhash();
-    const response = await connection.confirmTransaction(
+  public async waitForConfirmation(
+    signature: string,
+    commitment: Commitment = 'finalized',
+  ): Promise<SolanaRpcResponseAndContext<SolanaSignatureResult>> {
+    const latestBlockhash = await this.connection.getLatestBlockhash();
+    const response = await this.connection.confirmTransaction(
       {
         signature,
         blockhash: latestBlockhash.blockhash,
@@ -30,47 +65,91 @@ export class SolanaWalletProvider implements ISolanaWalletProvider {
       },
       commitment,
     );
-    console.log('Transaction confirmed:', response);
+
+    return response;
   }
 
-  public async sendTransaction(transaction: VersionedTransaction): Promise<Hash> {
-    const txHash = (await this.connection.sendTransaction(transaction)) as Hash;
-    await this.waitForConfirmation(txHash, this.connection);
+  /**
+   * Send a raw transaction to the Solana network.
+   * @param rawTransaction - The raw transaction to send.
+   * @returns The transaction signature.
+   */
+  public async sendTransaction(rawTransaction: Uint8Array | Array<number>): Promise<TransactionSignature> {
+    return this.connection.sendRawTransaction(rawTransaction);
+  }
+
+  /**
+   * Send a raw transaction to the Solana network and wait for confirmation.
+   * @param rawTransaction - The raw transaction to send.
+   * @param commitment - The commitment level to use. Defaults to 'finalized'.
+   * @returns The transaction signature.
+   */
+  public async sendTransactionWithConfirmation(
+    rawTransaction: Uint8Array | Array<number>,
+    commitment: Commitment = 'finalized',
+  ): Promise<TransactionSignature> {
+    const txHash = await this.connection.sendRawTransaction(rawTransaction);
+    await this.waitForConfirmation(txHash, commitment);
     return txHash;
   }
 
-  async buildV0Txn(instructions: TransactionInstruction[], signers: Keypair[]) {
-    const blockHash = await this.connection.getLatestBlockhash().then(res => res.blockhash);
+  /**
+   * Build a v0 versioned transaction.
+   * @param instructions - The instructions to include in the transaction.
+   * @param signers - The signers to include in the transaction.
+   * @returns The v0 transaction.
+   */
+  async buildV0Txn(rawInstructions: SolanaRawTransactionInstruction[]): Promise<SolanaSerializedTransaction> {
+    const instructions = this.buildTransactionInstruction(rawInstructions);
 
     const messageV0 = new TransactionMessage({
-      //@ts-ignore
-      payerKey: signers[0]?.publicKey,
-      recentBlockhash: blockHash,
+      payerKey: this.wallet.publicKey,
+      recentBlockhash: (await this.connection.getLatestBlockhash()).blockhash,
       instructions,
     }).compileToV0Message();
 
     const tx = new VersionedTransaction(messageV0);
-    tx.sign(signers);
-    return tx;
+    tx.sign([this.wallet]);
+
+    return tx.serialize();
   }
 
-  public getAddress(): PublicKey {
-    return this.wallet.publicKey;
-  }
-
-  public getWallet(): Keypair {
-    return this.wallet;
+  public getWalletBase58PublicKey(): SolanaBase58PublicKey {
+    return this.wallet.publicKey.toBase58();
   }
 
   public async getWalletAddress(): Promise<string> {
-    return this.getAddress().toBase58();
+    return this.getWalletBase58PublicKey();
   }
 
   public async getWalletAddressBytes(): Promise<Hex> {
-    return `0x${Buffer.from(await this.getWalletAddress()).toString('hex')}` as Hex;
+    return `0x${Buffer.from(await this.getWalletAddress()).toString('hex')}`;
   }
 
-  public async getAssociatedTokenAddress(mint: PublicKey): Promise<PublicKey> {
-    return await getAssociatedTokenAddress(mint, this.getAddress(), true);
+  public async getAssociatedTokenAddress(mint: SolanaBase58PublicKey): Promise<SolanaBase58PublicKey> {
+    return (await getAssociatedTokenAddress(new PublicKey(mint), this.wallet.publicKey, true)).toBase58();
+  }
+
+  public async getBalance(publicKey: SolanaBase58PublicKey): Promise<number> {
+    return await this.connection.getBalance(new PublicKey(publicKey));
+  }
+
+  public async getTokenAccountBalance(publicKey: SolanaBase58PublicKey): Promise<RpcResponseAndContext<TokenAmount>> {
+    return this.connection.getTokenAccountBalance(new PublicKey(publicKey));
+  }
+
+  private buildTransactionInstruction(rawInstructions: SolanaRawTransactionInstruction[]): TransactionInstruction[] {
+    return rawInstructions.map(
+      rawInstruction =>
+        new TransactionInstruction({
+          keys: rawInstruction.keys.map((key: SolanaAccountMeta) => ({
+            pubkey: new PublicKey(key.pubkey),
+            isSigner: key.isSigner,
+            isWritable: key.isWritable,
+          })),
+          programId: new PublicKey(rawInstruction.programId),
+          data: Buffer.from(rawInstruction.data),
+        }),
+    );
   }
 }
--- packages/sdk/src/entities/solana/Configs.ts
+++ packages/sdk/src/entities/solana/Configs.ts
@@ -1,10 +1,10 @@
 import * as anchor from '@coral-xyz/anchor';
-import { Connection, type Keypair, PublicKey } from '@solana/web3.js';
+import { Connection, PublicKey } from '@solana/web3.js';
 import type { AssetManager } from './types/asset_manager.js';
 import type { Connection as ConnectionContract } from './types/connection.js';
-export async function getProvider(keypair: Keypair, rpcUrl: string, wsUrl: string): Promise<anchor.AnchorProvider> {
+export async function getProvider(base58PublicKey: string, rpcUrl: string, wsUrl: string): Promise<anchor.AnchorProvider> {
   const wallet = {
-    publicKey: keypair.publicKey,
+    publicKey: new PublicKey(base58PublicKey),
     signTransaction: () => Promise.reject(),
     signAllTransactions: () => Promise.reject(),
   };
@@ -17,22 +17,27 @@ export async function getProvider(keypair: Keypair, rpcUrl: string, wsUrl: strin
 export async function getAssetManagerIdl(assetManager: string, provider: anchor.AnchorProvider) {
   try {
     const idl = await anchor.Program.fetchIdl(new PublicKey(assetManager), provider);
+
     if (!idl) {
       throw new Error('asset manager idl not available');
     }
+
     return idl;
   } catch (err) {
-    console.log('Failed to fetch Program IDl:', err);
+    console.error('Failed to fetch Program IDl:', err);
     throw err;
   }
 }
 
 export async function getConnectionIdl(connection: string, provider: anchor.AnchorProvider) {
   try {
+
     const idl = await anchor.Program.fetchIdl(new PublicKey(connection), provider);
+
     if (!idl) {
       throw new Error('asset manager idl not available');
     }
+
     return idl;
   } catch (err) {
     console.log('Failed to fetch Program IDl:', err);
@@ -41,25 +46,25 @@ export async function getConnectionIdl(connection: string, provider: anchor.Anch
 }
 
 export async function getAssetManagerProgram(
-    keypair: Keypair,
+    base58PublicKey: string,
     rpcUrl: string,
     wsUrl: string,
     assetManager: string,
 ): Promise<anchor.Program<AssetManager>> {
-  const provider = await getProvider(keypair, rpcUrl, wsUrl);
+  const provider = await getProvider(base58PublicKey, rpcUrl, wsUrl);
   const idl = await getAssetManagerIdl(assetManager, provider);
 
-  return new anchor.Program(idl as anchor.Idl, provider) as unknown as anchor.Program<AssetManager>;
+  return new anchor.Program(idl, provider) as unknown as anchor.Program<AssetManager>;
 }
 
 export async function getConnectionProgram(
-    keypair: Keypair,
+    base58PublicKey: string,
     rpcUrl: string,
     wsUrl: string,
     connection: string,
 ): Promise<anchor.Program<ConnectionContract>> {
-  const provider = await getProvider(keypair, rpcUrl, wsUrl);
+  const provider = await getProvider(base58PublicKey, rpcUrl, wsUrl);
   const idl = await getConnectionIdl(connection, provider);
 
-  return new anchor.Program(idl as anchor.Idl, provider) as unknown as anchor.Program<ConnectionContract>;
+  return new anchor.Program(idl, provider) as unknown as anchor.Program<ConnectionContract>;
 }
