f82ad8e9fb65ca08e4c0f69311027433eab2dc46
swiftcc624 <swiftcc624@gmail.com>
2025-07-04 14:07:12 +0800

    fix: remove dialog open state from sidebar waitlist button

--- apps/web/components/landing/sidebar.tsx
+++ apps/web/components/landing/sidebar.tsx
@@ -48,7 +48,6 @@ const Sidebar = ({
               href="#"
               onClick={() => {
                 toggle();
-                setIsDialogOpen(true);
               }}
             >
               <p>Join waitlist</p>
