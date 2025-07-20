dfd5b80ac72e09b829e565cc78ff2eda9736fa58
swiftcc624 <swiftcc624@gmail.com>
2025-07-21 02:36:59 +0800

    feat: support sonic as spoke chain

--- packages/dapp-kit/src/hooks/mm/useUserReservesData.ts
+++ packages/dapp-kit/src/hooks/mm/useUserReservesData.ts
@@ -1,15 +1,26 @@
 import { allXTokens } from '@/core';
-import { encodeAddress, EvmWalletAbstraction, getMoneyMarketConfig, type EvmHubProvider } from '@sodax/sdk';
+import {
+  encodeAddress,
+  EvmWalletAbstraction,
+  getMoneyMarketConfig,
+  SONIC_MAINNET_CHAIN_ID,
+  type SonicSpokeProvider,
+  SonicSpokeService,
+  type EvmHubProvider,
+} from '@sodax/sdk';
 import type { HubChainId, SpokeChainId } from '@sodax/types';
 import type { ChainId } from '@sodax/types';
 import { useQuery } from '@tanstack/react-query';
 import { useHubProvider } from '../provider/useHubProvider';
 import { useSodaxContext } from '../shared/useSodaxContext';
+import { useSpokeProvider } from '../provider/useSpokeProvider';
+import type { Address } from 'viem';
 
 export function useUserReservesData(spokeChainId: ChainId, address: string | undefined) {
   const { sodax } = useSodaxContext();
   const hubChainId = (sodax.config?.hubProviderConfig?.chainConfig.chain.id ?? 'sonic') as HubChainId;
   const hubProvider = useHubProvider();
+  const spokeProvider = useSpokeProvider(spokeChainId);
 
   const { data: userReserves } = useQuery({
     queryKey: ['userReserves', spokeChainId, address],
@@ -25,9 +36,14 @@ export function useUserReservesData(spokeChainId: ChainId, address: string | und
         hubProvider as EvmHubProvider,
       );
 
+      let userAddress: Address = hubWalletAddress;
+      if (spokeChainId === SONIC_MAINNET_CHAIN_ID) {
+        userAddress = await SonicSpokeService.getUserRouter(address as Address, spokeProvider as SonicSpokeProvider);
+      }
+
       const moneyMarketConfig = getMoneyMarketConfig(hubChainId);
       const [res] = await sodax.moneyMarket.getUserReservesData(
-        hubWalletAddress as `0x${string}`,
+        userAddress,
         moneyMarketConfig.uiPoolDataProvider,
         moneyMarketConfig.poolAddressesProvider,
       );
