17b450f9fd8ee6af9cf3b9ae2a3c6b7acc739724
swiftcc624 <swiftcc624@gmail.com>
2025-08-11 14:19:32 +0800

    refactor(migrate): replace NumberInput with Input

--- apps/web/app/(apps)/migrate/_components/currency-input-panel.tsx
+++ apps/web/app/(apps)/migrate/_components/currency-input-panel.tsx
@@ -5,6 +5,7 @@ import CurrencyLogoICX from './currency-logo-icx';
 import CurrencyLogoSoda from './currency-logo-soda';
 import { ICON_MAINNET_CHAIN_ID, type XToken, type SpokeChainId } from '@sodax/types';
 import { NumberInput } from '@/components/ui/number-input';
+import { Input } from '@/components/ui/input';
 import { formatUnits } from 'viem';
 
 export enum CurrencyInputPanelType {
@@ -78,14 +79,23 @@ const CurrencyInputPanel: React.FC<CurrencyInputPanelProps> = ({
         <div className="inline-flex gap-1 items-center">
           <div className="text-right justify-center text-espresso font-['InterRegular'] font-bold">
             <div className="relative">
-              <NumberInput
+              <Input
+                type="number"
+                ref={inputRef}
+                value={inputValue === '' ? '' : Number(inputValue)}
+                onChange={onInputChange}
+                onFocus={onInputFocus}
+                placeholder="0"
+                className="text-right border-none shadow-none focus:outline-none focus:ring-0 focus:border-none focus:shadow-none focus-visible:border-none focus-visible:ring-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-inner-spin-button]:m-0 !pr-0 focus:!text-espresso text-espresso !text-(size:--subtitle)"
+              />
+              {/* <NumberInput  
                 ref={inputRef}
                 value={inputValue === '' ? 0 : Number(inputValue)}
                 onChange={onInputChange}
                 onFocus={onInputFocus}
                 placeholder="0"
                 className="text-right border-none shadow-none focus:outline-none focus:ring-0 focus:border-none focus:shadow-none focus-visible:border-none focus-visible:ring-0 [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:m-0 [&::-webkit-inner-spin-button]:m-0 !pr-0 focus:!text-espresso text-espresso !text-(size:--subtitle)"
-              />
+              /> */}
             </div>
           </div>
           <div className="text-right justify-center text-espresso font-['InterRegular'] font-normal text-(size:--body-super-comfortable)">
