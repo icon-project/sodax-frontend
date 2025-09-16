77ad091f5f2a3261a3aa699e9d27a21dfc4926d0
0xmilktea <162528929+0xmilktea@users.noreply.github.com>
2025-09-16 16:17:23 +0800

    feat: add lightlink chain (#329)

--- apps/web/constants/chains.ts
+++ apps/web/constants/chains.ts
@@ -1,5 +1,4 @@
-// apps/web/constants/chains.ts
-import { SPOKE_CHAIN_IDS } from '@sodax/types';
+import { CHAIN_IDS } from '@sodax/types';
 
 export interface ChainUI {
   id: string;
@@ -12,18 +11,18 @@ export interface ChainUI {
  * Maps SPOKE_CHAIN_IDS to human-readable names and icon paths
  */
 export const availableChains: ChainUI[] = [
-  { id: SPOKE_CHAIN_IDS[0], name: 'Sonic', icon: '/chain/sonic.png' },
-  { id: SPOKE_CHAIN_IDS[1], name: 'Avalanche', icon: '/chain/0xa86a.avax.png' },
-  { id: SPOKE_CHAIN_IDS[2], name: 'Arbitrum', icon: '/chain/0xa4b1.arbitrum.png' },
-  { id: SPOKE_CHAIN_IDS[3], name: 'Base', icon: '/chain/0x2105.base.png' },
-  { id: SPOKE_CHAIN_IDS[4], name: 'BSC', icon: '/chain/0x38.bsc.png' },
-  { id: SPOKE_CHAIN_IDS[5], name: 'Injective', icon: '/chain/injective-1.png' },
-  { id: SPOKE_CHAIN_IDS[6], name: 'Sui', icon: '/chain/sui.png' },
-  { id: SPOKE_CHAIN_IDS[7], name: 'Optimism', icon: '/chain/0xa.optimism.png' },
-  { id: SPOKE_CHAIN_IDS[8], name: 'Polygon', icon: '/chain/0x89.polygon.png' },
-  { id: SPOKE_CHAIN_IDS[9], name: 'Solana', icon: '/chain/solana.png' },
-  { id: SPOKE_CHAIN_IDS[11], name: 'Stellar', icon: '/chain/stellar.png' },
-  { id: SPOKE_CHAIN_IDS[10], name: 'Icon', icon: '/chain/0x1.icon.png' },
+  { id: CHAIN_IDS[0], name: 'Sonic', icon: '/chain/sonic.png' },
+  { id: CHAIN_IDS[1], name: 'Avalanche', icon: '/chain/0xa86a.avax.png' },
+  { id: CHAIN_IDS[2], name: 'Arbitrum', icon: '/chain/0xa4b1.arbitrum.png' },
+  { id: CHAIN_IDS[3], name: 'Base', icon: '/chain/0x2105.base.png' },
+  { id: CHAIN_IDS[4], name: 'BSC', icon: '/chain/0x38.bsc.png' },
+  { id: CHAIN_IDS[5], name: 'Injective', icon: '/chain/injective-1.png' },
+  { id: CHAIN_IDS[6], name: 'Sui', icon: '/chain/sui.png' },
+  { id: CHAIN_IDS[7], name: 'Optimism', icon: '/chain/0xa.optimism.png' },
+  { id: CHAIN_IDS[8], name: 'Polygon', icon: '/chain/0x89.polygon.png' },
+  { id: CHAIN_IDS[9], name: 'Solana', icon: '/chain/solana.png' },
+  { id: CHAIN_IDS[11], name: 'Stellar', icon: '/chain/stellar.png' },
+  { id: CHAIN_IDS[10], name: 'Icon', icon: '/chain/0x1.icon.png' },
   // { id: SPOKE_CHAIN_IDS[12], name: 'Nibiru', icon: '/chain/nibiru.png' },
 ];
 
