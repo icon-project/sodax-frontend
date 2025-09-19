2c310c6103e287262c3f17e7d1a7649dcb57f8ce
swiftcc624 <swiftcc624@gmail.com>
2025-09-19 10:51:13 +0800

    feat: enable loans page temporarily

--- apps/web/components/shared/route-tabs.tsx
+++ apps/web/components/shared/route-tabs.tsx
@@ -34,7 +34,7 @@ export const tabConfigs: TabConfig[] = [
     type: 'loans',
     label: 'Loans',
     content: 'a quick loans',
-    enabled: false,
+    enabled: true,
   },
   {
     value: 'migrate',
