908fc1df59f31a0bf7c54f598de35ee674ce7202
swiftcc624 <swiftcc624@gmail.com>
2025-10-15 09:36:19 +0800

    fix: bnUSD chain selector

--- apps/web/constants/chains.ts
+++ apps/web/constants/chains.ts
@@ -1,4 +1,18 @@
-import { CHAIN_IDS } from '@sodax/types';
+import {
+  ARBITRUM_MAINNET_CHAIN_ID,
+  AVALANCHE_MAINNET_CHAIN_ID,
+  BASE_MAINNET_CHAIN_ID,
+  BSC_MAINNET_CHAIN_ID,
+  INJECTIVE_MAINNET_CHAIN_ID,
+  LIGHTLINK_MAINNET_CHAIN_ID,
+  OPTIMISM_MAINNET_CHAIN_ID,
+  POLYGON_MAINNET_CHAIN_ID,
+  SONIC_MAINNET_CHAIN_ID,
+  SUI_MAINNET_CHAIN_ID,
+  SOLANA_MAINNET_CHAIN_ID,
+  STELLAR_MAINNET_CHAIN_ID,
+  ICON_MAINNET_CHAIN_ID,
+} from '@sodax/types';
 
 export interface ChainUI {
   id: string;
@@ -11,19 +25,20 @@ export interface ChainUI {
  * Maps SPOKE_CHAIN_IDS to human-readable names and icon paths
  */
 export const availableChains: ChainUI[] = [
-  { id: CHAIN_IDS[0], name: 'Sonic', icon: '/chain/sonic.png' },
-  { id: CHAIN_IDS[1], name: 'Avalanche', icon: '/chain/0xa86a.avax.png' },
-  { id: CHAIN_IDS[2], name: 'Arbitrum', icon: '/chain/0xa4b1.arbitrum.png' },
-  { id: CHAIN_IDS[3], name: 'Base', icon: '/chain/0x2105.base.png' },
-  { id: CHAIN_IDS[4], name: 'BSC', icon: '/chain/0x38.bsc.png' },
-  { id: CHAIN_IDS[5], name: 'Injective', icon: '/chain/injective-1.png' },
-  { id: CHAIN_IDS[6], name: 'Sui', icon: '/chain/sui.png' },
-  { id: CHAIN_IDS[7], name: 'Optimism', icon: '/chain/0xa.optimism.png' },
-  { id: CHAIN_IDS[8], name: 'Polygon', icon: '/chain/0x89.polygon.png' },
-  { id: CHAIN_IDS[9], name: 'Solana', icon: '/chain/solana.png' },
-  { id: CHAIN_IDS[11], name: 'Stellar', icon: '/chain/stellar.png' },
-  { id: CHAIN_IDS[10], name: 'Icon', icon: '/chain/0x1.icon.png' },
-  // { id: SPOKE_CHAIN_IDS[12], name: 'Nibiru', icon: '/chain/nibiru.png' },
+  { id: SONIC_MAINNET_CHAIN_ID, name: 'Sonic', icon: '/chain/sonic.png' },
+  { id: AVALANCHE_MAINNET_CHAIN_ID, name: 'Avalanche', icon: '/chain/0xa86a.avax.png' },
+  { id: ARBITRUM_MAINNET_CHAIN_ID, name: 'Arbitrum', icon: '/chain/0xa4b1.arbitrum.png' },
+  { id: BASE_MAINNET_CHAIN_ID, name: 'Base', icon: '/chain/0x2105.base.png' },
+  { id: BSC_MAINNET_CHAIN_ID, name: 'BSC', icon: '/chain/0x38.bsc.png' },
+  { id: INJECTIVE_MAINNET_CHAIN_ID, name: 'Injective', icon: '/chain/injective-1.png' },
+  { id: SUI_MAINNET_CHAIN_ID, name: 'Sui', icon: '/chain/sui.png' },
+  { id: OPTIMISM_MAINNET_CHAIN_ID, name: 'Optimism', icon: '/chain/0xa.optimism.png' },
+  { id: POLYGON_MAINNET_CHAIN_ID, name: 'Polygon', icon: '/chain/0x89.polygon.png' },
+  { id: SOLANA_MAINNET_CHAIN_ID, name: 'Solana', icon: '/chain/solana.png' },
+  { id: STELLAR_MAINNET_CHAIN_ID, name: 'Stellar', icon: '/chain/stellar.png' },
+  { id: ICON_MAINNET_CHAIN_ID, name: 'Icon', icon: '/chain/0x1.icon.png' },
+  { id: LIGHTLINK_MAINNET_CHAIN_ID, name: 'Lightlink', icon: '/chain/lightlink.png' },
+  // { id: NIBIRU_MAINNET_CHAIN_ID, name: 'Nibiru', icon: '/chain/nibiru.png' },
 ];
 
 /**
