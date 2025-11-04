a6a485832e01b089cd658e5f7e464e2e7a9b85fc
swiftcc624 <56027533+swiftcc624@users.noreply.github.com>
2025-11-04 09:47:48 +0800

    fix: page transition (#474)

--- apps/web/app/(apps)/layout.tsx
+++ apps/web/app/(apps)/layout.tsx
@@ -9,38 +9,76 @@ import { WalletModal } from '@/components/shared/wallet-modal2/wallet-modal';
 import TermsConfirmationModal from '@/components/shared/wallet-modal2/terms-confirmation-modal';
 import { ModalStoreProvider } from '@/stores/modal-store-provider';
 import '../globals.css';
-import { AppStoreProvider } from '@/stores/app-store-provider';
+import { useAppStore } from '@/stores/app-store-provider';
+import { motion } from 'framer-motion';
+import LandingPage from '../page';
+import { headerVariants, contentVariants, mainContentVariants } from '@/constants/animation';
+import { useRef, useState, useLayoutEffect } from 'react';
+import { usePathname } from 'next/navigation';
+import { useIsMobile } from '@/hooks/use-mobile';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <AppStoreProvider>
-      <SwapStoreProvider>
-        <MigrationStoreProvider>
-          <ModalStoreProvider>
-            <div className="bg-cream-white min-h-screen pb-24 md:pb-0 w-screen overflow-x-hidden">
-              <Header />
+  const { isSwitchingPage } = useAppStore(state => state);
+  const ref = useRef<HTMLDivElement>(null);
+  const [height, setHeight] = useState(0);
+  const pathname = usePathname();
+  const isMobile = useIsMobile();
 
-              <div className="w-full lg:w-[1024px] lg:max-w-[1024px] mx-auto md:-mt-34 -mt-36">
-                <div className="flex justify-center items-start min-h-[calc(100vh-192px)] md:min-h-[calc(100vh-224px)]">
+  useLayoutEffect(() => {
+    const calculateHeight = (): void => {
+      if (pathname !== '/' && ref.current) {
+        setHeight(ref.current.offsetHeight);
+      }
+    };
+
+    calculateHeight();
+  }, [pathname]);
+
+  return (
+    <SwapStoreProvider>
+      <MigrationStoreProvider>
+        <div className="fixed top-0 left-0 w-screen h-screen">
+          <LandingPage />
+        </div>
+        <ModalStoreProvider>
+          <div className="min-h-screen w-[100%] overflow-hidden">
+            <motion.div variants={headerVariants} initial="closed" animate={isSwitchingPage ? 'open' : 'closed'}>
+              <Header />
+            </motion.div>
+
+            <motion.div
+              variants={contentVariants}
+              initial={{ y: '300px' }}
+              animate={isSwitchingPage ? 'open' : 'closed'}
+              className="bg-cream-white relative"
+              style={{ height: !isMobile ? height - 136 : height - 40 }}
+            >
+              <div className="w-full lg:w-[1024px] lg:max-w-[1024px] absolute md:-top-34 -top-36 left-1/2 -translate-x-1/2">
+                <motion.div
+                  variants={mainContentVariants}
+                  initial={{ y: 30, opacity: 0 }}
+                  animate={isSwitchingPage ? 'open' : 'closed'}
+                  className="flex justify-center items-start min-h-[calc(100vh-192px)] md:min-h-[calc(100vh-224px)]"
+                >
                   <RouteTabs />
                   <div
+                    ref={ref}
                     className="w-full md:w-[calc(100%-200px)] lg:w-[784px] min-h-[calc(100vh-192px)] md:min-h-[calc(100vh-104px)]
                         p-[80px_16px] pb-10 md:p-[120px_48px] lg:p-[120px_80px] flex items-start gap-2
                         rounded-tl-[32px] rounded-tr-[32px] border-8 border-vibrant-white bg-[radial-gradient(239.64%_141.42%_at_0%_0%,_#E3D8D8_0%,_#F5F2F2_22.12%,_#F5F2F2_57.69%,_#F5EDED_100%)]
-                        border-b-0 z-20 ml-0 md:-ml-16"
-                    style={{ backgroundColor: '#F5F2F2' }}
+                        border-b-0 z-20 ml-0 md:-ml-16 max-h-[calc(100vh-192px)] sm:max-h-none overflow-auto"
                   >
                     {children}
                   </div>
-                </div>
+                </motion.div>
               </div>
+            </motion.div>
 
-              <WalletModal />
-              <TermsConfirmationModal />
-            </div>
-          </ModalStoreProvider>
-        </MigrationStoreProvider>
-      </SwapStoreProvider>
-    </AppStoreProvider>
+            <WalletModal />
+            <TermsConfirmationModal />
+          </div>
+        </ModalStoreProvider>
+      </MigrationStoreProvider>
+    </SwapStoreProvider>
   );
 }
---
+++ apps/web/constants/animation.ts
@@ -0,0 +1,70 @@
+import { stagger } from 'motion/react';
+
+export const headerVariants = {
+  open: {
+    opacity: 1,
+    transition: { duration: 1, delay: 0.5 },
+  },
+  closed: {
+    opacity: 0,
+    transition: { duration: 1 },
+  },
+};
+
+export const contentVariants = {
+  open: {
+    opacity: 1,
+    transition: { duration: 3, delay: 0.5, y: { stiffness: 1000, velocity: -100 } },
+    y: 0,
+  },
+  closed: {
+    opacity: 0.5,
+    transition: { duration: 1, y: { stiffness: 1000 } },
+    y: '75vh',
+  },
+};
+
+export const mainContentVariants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      opacity: { delay: 0.5, duration: 0.3 },
+      y: { delay: 0.5, duration: 0.2, velocity: -100 },
+    },
+  },
+  closed: {
+    opacity: 0.5,
+    y: '75vh',
+    transition: {
+      duration: 1,
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
+export const itemVariants = {
+  open: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      y: { stiffness: 1000, velocity: -100 },
+    },
+  },
+  closed: {
+    y: 50,
+    opacity: 0,
+    transition: {
+      y: { stiffness: 1000 },
+    },
+  },
+};
+
+export const listVariants = {
+  open: {
+    transition: { delayChildren: stagger(0.07, { startDelay: 0.2 }) },
+  },
+  closed: {
+    transition: { delayChildren: stagger(0.05, { from: 'last' }) },
+  },
+};
