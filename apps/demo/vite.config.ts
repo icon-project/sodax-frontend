bef1022a919c1403a6811ba580cf46373be437f9
Malgorzata (Gosia) Stano <stano.malgorzata@gmail.com>
2025-11-04 17:11:57 +0100

    fix: upgaded vite config with tailwind

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import type { ConfigEnv } from 'vite';
 import { defineConfig, loadEnv } from 'vite';
 import { nodePolyfills } from '@bangjelkoski/vite-plugin-node-polyfills';
-
+import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 
@@ -14,6 +14,7 @@ export default defineConfig(({ command, mode }: ConfigEnv) => {
     // depending on your application, base can also be "/"
     base: '/',
     plugins: [
+      tailwindcss(),
       svgr({
         include: '**/*.svg',
         svgrOptions: {
