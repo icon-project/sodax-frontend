fa0ff471a61691a669afb9dc86f577892031c95a
MarkacRobi <robi.markac@gmail.com>
2025-11-05 19:44:03 +0100

    chore: update api urls

--- apps/web/providers/constants.ts
+++ apps/web/providers/constants.ts
@@ -40,7 +40,7 @@ export const partnerFeePercentage = {
 
 export const mainnetSolverConfig = {
   intentsContract: '0x6382D6ccD780758C5e8A6123c33ee8F4472F96ef',
-  solverApiEndpoint: 'https://sodax-solver.iconblockchain.xyz',
+  solverApiEndpoint: 'https://api.sodax.com/v1/intent',
   partnerFee: partnerFeePercentage, // fee to be paid to the partner address
 } satisfies SolverConfigParams;
 
