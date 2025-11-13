a9dca321f6e86c76b2a66d08f937c21a1f8cc119
swiftcc624 <swiftcc624@gmail.com>
2025-11-13 10:38:49 +0800

    refactor(wallet-modal): enhance wallet item layout and conditional rendering

--- apps/web/components/shared/wallet-modal2/wallet-item.tsx
+++ apps/web/components/shared/wallet-modal2/wallet-item.tsx
@@ -62,34 +62,68 @@ export const WalletItem: React.FC<WalletItemProps> = ({
         onClick={handleConnect}
       >
         <div className="flex flex-col gap-4 w-full">
-          <div className="flex justify-between items-center gap-4 w-full">
-            <div className="flex justify-start items-center gap-4">
-              <div
-                data-property-1="Active"
-                className="rounded-[6px] inline-flex flex-col justify-center items-center overflow-hidden z-51"
+          {isEVM && (
+            <div className="flex justify-between items-center gap-4 w-full">
+              <div className="flex justify-start items-center gap-4">
+                <div
+                  data-property-1="Active"
+                  className="rounded-[6px] inline-flex flex-col justify-center items-center overflow-hidden z-51"
+                >
+                  <Image
+                    src={xConnector.icon?.trim() || ''}
+                    alt={xConnector.name}
+                    width={24}
+                    height={24}
+                    className=""
+                  />
+                </div>
+                <div className="justify-center text-espresso text-(length:--body-comfortable) font-medium font-['InterRegular'] leading-tight group-hover:font-bold">
+                  {isPending ? 'Waiting for wallet' : xConnector.name}
+                </div>
+              </div>
+              <Button
+                className={`w-6 h-6 p-0 rounded-full  text-espresso hover:bg-cherry-bright hover:text-white cursor-pointer ${isPending === true ? 'bg-cherry-brighter' : 'bg-cream'}  disabled:opacity-100 disabled:pointer-events-auto`}
+                onClick={handleConnect}
+                disabled={isPending}
               >
-                <Image src={xConnector.icon?.trim() || ''} alt={xConnector.name} width={24} height={24} className="" />
-              </div>
-              <div className="justify-center text-espresso text-(length:--body-comfortable) font-medium font-['InterRegular'] leading-tight group-hover:font-bold">
-                {isPending ? 'Waiting for wallet' : xConnector.name}
-              </div>
+                {isPending && <Loader2 className="animate-spin" />}
+                {!isPending && <PlusIcon className="w-4 h-4" />}
+              </Button>
             </div>
-            <Button
-              className={`w-6 h-6 p-0 rounded-full  text-espresso hover:bg-cherry-bright hover:text-white cursor-pointer ${isPending === true ? 'bg-cherry-brighter' : 'bg-cream'}  disabled:opacity-100 disabled:pointer-events-auto`}
-              onClick={handleConnect}
-              disabled={isPending}
-            >
-              {isPending && <Loader2 className="animate-spin" />}
-              {!isPending && <PlusIcon className="w-4 h-4" />}
-            </Button>
-          </div>
+          )}
 
           <div className="flex justify-start items-center flex-wrap ">
-            <div className="flex justify-start items-center flex-wrap">
+            <div className="flex justify-start items-center flex-wrap w-full">
               {!isEVM && (
-                <div className="w-6 relative rounded-[6px] shadow-[4px_0px_4px_rgba(175,145,145)] outline-4 outline-white inline-flex flex-col justify-start items-start overflow-hidden ml-1">
-                  <Image src={icon} alt={name} width={24} height={24} className="rounded-[6px]" />
-                  <div className="w-6 h-6 left-0 top-0 absolute bg-[radial-gradient(ellipse_50.00%_50.00%_at_50.00%_50.00%,_rgba(237,_230,_230,_0.40)_0%,_rgba(237.22,_230.40,_230.40,_0.60)_100%)]" />
+                <div className="flex align-center justify-between w-full">
+                  <div className="flex items-center">
+                    <div className="w-6 relative rounded-[6px] shadow-[4px_0px_4px_rgba(175,145,145)] outline-4 outline-white inline-flex flex-col justify-start items-start overflow-hidden ml-1">
+                      <Image src={icon} alt={name} width={24} height={24} className="rounded-[6px]" />
+                    </div>
+                    <div
+                      data-property-1="Active"
+                      className="rounded-[6px] inline-flex flex-col justify-center items-center overflow-hidden z-51 shadow-[4px_0px_4px_rgba(175,145,145)] outline-4 outline-white"
+                    >
+                      <Image
+                        src={xConnector.icon?.trim() || ''}
+                        alt={xConnector.name}
+                        width={24}
+                        height={24}
+                        className=""
+                      />
+                    </div>
+                    <div className="text-espresso text-(length:--body-comfortable) font-medium font-['InterRegular'] leading-tight group-hover:font-bold ml-4">
+                      {isPending ? 'Waiting for wallet' : xConnector.name}
+                    </div>
+                  </div>
+                  <Button
+                    className={`w-6 h-6 p-0 rounded-full  text-espresso hover:bg-cherry-bright hover:text-white cursor-pointer ${isPending === true ? 'bg-cherry-brighter' : 'bg-cream'}  disabled:opacity-100 disabled:pointer-events-auto`}
+                    onClick={handleConnect}
+                    disabled={isPending}
+                  >
+                    {isPending && <Loader2 className="animate-spin" />}
+                    {!isPending && <PlusIcon className="w-4 h-4" />}
+                  </Button>
                 </div>
               )}
               {isEVM && (
