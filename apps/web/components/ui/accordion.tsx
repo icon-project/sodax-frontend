33f3aff9432109f66924826d908a1ea851c4376b
swiftcc624 <swiftcc624@gmail.com>
2025-12-04 10:52:31 +0800

    fix: update token accordion item layout by adding margin-top to ChevronDownIcon for improved spacing

--- apps/web/components/ui/accordion.tsx
+++ apps/web/components/ui/accordion.tsx
@@ -63,14 +63,14 @@ function AccordionTriggerWithButton({
       <AccordionPrimitive.Trigger
         data-slot="accordion-trigger"
         className={cn(
-          'focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 justify-between gap-4 rounded-md text-left text-sm font-medium transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>button]:!outline-[#e5dbdb] [&[data-state=open]>button]:rotate-180 [&[data-state=open]>svg]:rotate-180 items-center py-0',
+          'focus-visible:border-ring focus-visible:ring-ring/50 flex flex-1 justify-between gap-4 rounded-md text-left text-sm font-medium transition-all outline-none focus-visible:ring-[3px] disabled:pointer-events-none disabled:opacity-50 [&[data-state=open]>button]:!outline-[#e5dbdb] [&[data-state=open]>svg]:mt-0 [&[data-state=open]>button]:rotate-180 [&[data-state=open]>svg]:rotate-180 items-center py-0',
           className,
         )}
         {...props}
       >
         {children}
         {isMobile ? (
-          <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
+          <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200 mt-4" />
         ) : (
           <Button
             variant="outline"
