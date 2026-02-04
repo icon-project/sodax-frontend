a4c5e1aad110f6caef9c4a569691307e7fdcf3af
swiftcc624 <swiftcc624@gmail.com>
2026-02-05 07:33:49 +0800

    feat(web): wip stake page

--- apps/web/app/(apps)/stake/page.tsx
+++ apps/web/app/(apps)/stake/page.tsx
@@ -1,5 +1,3 @@
-// apps/web/app/(apps)/stake/page.tsx
-
 'use client';
 
 import { StakeHeader, StakeInputPanel, StakeStatsCard, UnstakeModeToggle } from './_components';
@@ -7,6 +5,7 @@ import { useStakeActions, useStakeState } from './_stores/stake-store-provider';
 import { STAKE_MODE } from './_stores/stake-store';
 import { UnstakeRequests } from './_components/unstake-requests';
 import { STAKING_APR } from './_components/constants';
+import Tip from './_components/icons/tip';
 
 export default function StakePage(): React.JSX.Element {
   const { stakeMode } = useStakeState();
@@ -17,7 +16,11 @@ export default function StakePage(): React.JSX.Element {
 
       <div className="w-full rounded-(--layout-container-radius) outline-[#e4dada] outline outline-[3px] outline-offset-[-3px] flex flex-col justify-start items-start gap-2">
         <StakeInputPanel />
-        <div className="p-(--layout-space-big) w-full flex flex-col justify-start items-start bg-almost-white mix-blend-multiply rounded-bl-(--layout-container-radius) rounded-br-(--layout-container-radius)">
+        <div className="relative p-(--layout-space-big) w-full flex flex-col justify-start items-start bg-almost-white mix-blend-multiply rounded-bl-(--layout-container-radius) rounded-br-(--layout-container-radius)">
+          <div className="absolute top-0 left-[72px]">
+            <Tip fill="white" />
+          </div>
+
           <StakeStatsCard />
           <UnstakeModeToggle
             enabled={stakeMode === STAKE_MODE.UNSTAKING}
