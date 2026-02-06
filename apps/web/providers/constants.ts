4db6bdefc79ec72c7d46b9506854fd7a0929dd6d
Malgorzata (Gosia) Stano <92160569+gosiast@users.noreply.github.com>
2026-02-06 14:30:06 +0100

    fix(web): apps navigation (#795)

--- apps/web/providers/constants.ts
+++ apps/web/providers/constants.ts
@@ -24,9 +24,8 @@ export function chainIdToChainName(chainId: SpokeChainId): string {
 
 const testnet = process.env.NEXT_PUBLIC_TESTNET === 'YES';
 const hubChainId = SONIC_MAINNET_CHAIN_ID;
-// TODO gosia revert when testing otherwise won't work
-const hubRpcUrl = 'https://sonic-rpc.publicnode.com';
-// const hubRpcUrl = 'https://sonic-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg';
+// const hubRpcUrl = 'https://sonic-rpc.publicnode.com'; use this if in dev it isn't working
+const hubRpcUrl = 'https://sonic-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg';
 const hubConfig = {
   hubRpcUrl,
   chainConfig: getHubChainConfig(),
@@ -57,15 +56,15 @@ export const sodaxConfig = {
 
 export const rpcConfig: RpcConfig = {
   // EVM chains
-  // sonic: 'https://sonic-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
-  // '0xa86a.avax': 'https://avax-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
-  // '0xa4b1.arbitrum': 'https://arb-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
-  // '0x2105.base': 'https://base-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
-  // '0x38.bsc': 'https://bnb-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
-  // '0xa.optimism': 'https://opt-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
-  // '0x89.polygon': 'https://polygon-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
-  // ethereum: 'https://eth-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
-  // hyper: 'https://hyperliquid-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
+  sonic: 'https://sonic-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
+  '0xa86a.avax': 'https://avax-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
+  '0xa4b1.arbitrum': 'https://arb-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
+  '0x2105.base': 'https://base-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
+  '0x38.bsc': 'https://bnb-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
+  '0xa.optimism': 'https://opt-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
+  '0x89.polygon': 'https://polygon-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
+  ethereum: 'https://eth-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
+  hyper: 'https://hyperliquid-mainnet.g.alchemy.com/v2/fnxOcaJJQBJZeMMFpLqwg',
 
   // Other chains
   '0x1.icon': 'https://ctz.solidwallet.io/api/v3',
