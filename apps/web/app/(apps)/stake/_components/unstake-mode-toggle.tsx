78bff5d8f76dc0a30a75e69451826f19c0f00e1b
swiftcc624 <swiftcc624@gmail.com>
2026-02-11 20:59:46 +0800

    fix(web): basic design qa of stake page

--- apps/web/app/(apps)/stake/_components/unstake-mode-toggle.tsx
+++ apps/web/app/(apps)/stake/_components/unstake-mode-toggle.tsx
@@ -2,6 +2,8 @@
 
 import type React from 'react';
 import { Switch } from '@/components/ui/switch';
+import { useStakeState } from '../_stores/stake-store-provider';
+import { cn } from '@/lib/utils';
 
 interface UnstakeModeToggleProps {
   enabled: boolean;
@@ -9,8 +11,9 @@ interface UnstakeModeToggleProps {
 }
 
 export function UnstakeModeToggle({ enabled, onToggle }: UnstakeModeToggleProps): React.JSX.Element {
+  const { userXSodaBalance } = useStakeState();
   return (
-    <div className="flex items-center gap-2">
+    <div className={cn('flex items-center gap-2', userXSodaBalance === 0n && 'opacity-60 pointer-events-none')}>
       {/* <div className="justify-center text-clay text-xs font-normal font-['Inter'] leading-5">Unstake mode</div> */}
       <div className="justify-center text-espresso text-(length:--body-comfortable) font-normal font-['InterRegular'] leading-5">
         Unstake
