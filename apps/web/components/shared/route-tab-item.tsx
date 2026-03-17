625f98f565c2a882f5513634394b2deb26c06bbf
Gosia <stano.malgorzata@gmail.com>
2026-03-17 17:42:06 +0100

    fix: updated cursor-default on disabled navigation

--- apps/web/components/shared/route-tab-item.tsx
+++ apps/web/components/shared/route-tab-item.tsx
@@ -103,7 +103,7 @@ const RouteTabItem: React.FC<RouteTabItemProps> = ({
     return (
       <div
         ref={setRef ? (el: HTMLDivElement | null) => setRef(el as HTMLAnchorElement | null) : undefined}
-        className="cursor-not-allowed py-0 px-0"
+        className="cursor-default py-0 px-0"
       >
         {content}
       </div>
