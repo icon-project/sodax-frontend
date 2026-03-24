5eeb77c98a46708d4386da583841a7ab43dac7df
swiftcc624 <swiftcc624@gmail.com>
2026-03-24 11:10:56 +0800

    feat(web): integrate Wagmi state management

--- apps/web/providers/providers.tsx
+++ apps/web/providers/providers.tsx
@@ -1,20 +1,24 @@
 'use client';
 
 import type { ReactNode } from 'react';
-
 import { QueryClientProvider } from '@tanstack/react-query';
 import { SodaxProvider } from '@sodax/dapp-kit';
 import { sodaxConfig, rpcConfig } from './constants';
 import { getQueryClient } from '@/app/get-query-client';
 import { SodaxWalletProvider } from '@sodax/wallet-sdk-react';
+import type { State as WagmiState } from 'wagmi';
 
-export default function Providers({ children }: { children: ReactNode }) {
+export default function Providers({ children, initialState }: { children: ReactNode; initialState?: WagmiState }) {
   const queryClient = getQueryClient();
 
   return (
     <SodaxProvider testnet={false} config={sodaxConfig} rpcConfig={rpcConfig}>
       <QueryClientProvider client={queryClient}>
-        <SodaxWalletProvider rpcConfig={rpcConfig} options={{ wagmi: { ssr: true, reconnectOnMount: true } }}>
+        <SodaxWalletProvider
+          rpcConfig={rpcConfig}
+          options={{ wagmi: { ssr: true, reconnectOnMount: true } }}
+          initialState={initialState}
+        >
           {children}
         </SodaxWalletProvider>
       </QueryClientProvider>
