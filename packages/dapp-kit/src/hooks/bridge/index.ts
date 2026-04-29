d2e16791648face3aebb6cb68c71b647d3f2a644
Robi <robi.markac@gmail.com>
2026-04-29 13:47:34 +0200

    feat(dapp-kit,demo): bridge v2 (#1220)

--- packages/dapp-kit/src/hooks/bridge/index.ts
+++ packages/dapp-kit/src/hooks/bridge/index.ts
@@ -1,8 +1,5 @@
-/*
+export * from './useBridge.js';
 export * from './useBridgeAllowance.js';
 export * from './useBridgeApprove.js';
-export * from './useBridge.js';
 export * from './useGetBridgeableAmount.js';
 export * from './useGetBridgeableTokens.js';
-
-*/
